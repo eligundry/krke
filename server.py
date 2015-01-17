32ce5eef3e8cbc18acf7bde032c3456090c8e636
Eli Gundry <eligundry@gmail.com>
2015-01-17 01:22:38 -0500

    Installed twitter bootstrap

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 import requests as rekt
 
 MUSIXMATCH_API_KEY = 'a87d5804cbe38a1afd0de1dc263cbea1'
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def home():
-    return 'home'
+    return render_template('home.html')
 
 @app.route('/search')
 def search():
@@ -25,4 +27,5 @@ def lyrics():
     return 'lyrics'
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
